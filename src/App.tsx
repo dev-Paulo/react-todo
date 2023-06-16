f9d479f5fb796c858bca1d31a2605b572b403b1b
Paulo Renato <dev_paulo@outlook.com>
2023-06-16 10:17:19 -0300

    responsividade aplicada

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import logoTodo from "./assets/Logo.svg";
 import "./App.css";
-import { PlusCircle, Trash } from "@phosphor-icons/react";
+import { ClipboardText, PlusCircle, Trash } from "@phosphor-icons/react";
 import { FormEvent, useEffect, useState } from "react";
 
 function App() {
@@ -47,15 +47,15 @@ function App() {
   };
 
   return (
-    <div className="h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <header className="bg-gray-700 h-64 flex justify-center items-center">
         <div className="h-10">
           <img src={logoTodo} alt="Logo" />
         </div>
       </header>
-      <section className="bg-gray-600 flex-grow flex-col flex">
-        <div className="flex-col mt-[-20px] items-center flex">
-          <form onSubmit={handleAddTask} className="flex-row justify-center items-center flex w-2/4">
+      <section className="bg-gray-600 flex-grow flex-col flex px-4">
+        <div className="flex-col mt-[-20px] items-center flex ">
+          <form onSubmit={handleAddTask} className="flex-row justify-center items-center flex w-full md:w-2/4 ">
             <input
               type="text"
               name="taskTitle"
@@ -66,27 +66,28 @@ function App() {
             />
             <button
               type="submit"
-              className="text-center inline-flex items-center text-white bg-blue-600 hover:bg-blue-700 focus:ring-2 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 ml-2 focus:outline-none dark:focus:ring-blue-800"
+              className="text-center inline-flex items-center text-white bg-blue-600 hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 ml-2 focus:outline-none dark:focus:ring-blue-800"
             >
               Criar
               <PlusCircle size={20} className="ml-2" />
             </button>
           </form>
-          <div className="flex-row inline-flex justify-between mt-10 w-2/4 mb-4">
+          <div className="flex-row inline-flex justify-between mt-10 w-full md:w-2/4 mb-4">
             <div className="flex-row inline-flex items-center w-2/4">
               <p className="text-blue-500 font-bold mr-2">Tarefas criadas</p>
               <span className="bg-gray-300 text-white text-xs font-medium px-2.5 py-0.5 rounded-full">
                 {tasks.length}
               </span>
             </div>
-            <div className="flex-row inline-flex items-center justify-end w-2/4">
+            <div className="flex-row inline-flex items-center justify-end w-full md:w-2/4">
               <p className="text-purple-purple font-bold mr-2">Concluídas</p>
               <span className="bg-gray-300 text-white text-xs font-medium px-2.5 py-0.5 rounded-full">
                 {completedTasksCount} de {tasks.length}
               </span>
             </div>
           </div>
-          <div className="flex-col items-center flex w-2/4">
+          {tasks.length > 0 ? (
+            <div className="flex-col items-center flex w-full md:w-2/4">
             {tasks.map((task: Task, index: number) => (
               <div key={task.id} className="h-[72px] mb-4 p-4 rounded-lg bg-gray-500 flex-row inline-flex items-center w-full justify-between">
                 <div className="flex-row inline-flex items-center">
@@ -94,7 +95,7 @@ function App() {
                     id={`checkbox-${task.id}`}
                     type="checkbox"
                     value=""
-                    className="w-5 h-5 mr-2 text-purple-dark bg-gray-500 border-blue-500 rounded-xl focus:ring-purple-purple focus:ring-3 ring-blue-500"
+                    className="w-5 h-5 mr-2 text-purple-dark bg-gray-500 border-blue-500 rounded-xl focus:ring-purple-purple focus:ring-3 ring-blue-500 cursor-pointer"
                     onChange={() => handleCheckboxChange(index)}
                   />
                   <p
@@ -108,11 +109,21 @@ function App() {
                   </p>
                 </div>
                 <div className="">
-                  <Trash size={24} className="text-gray-300" onClick={() => handleTaskDeletion(index)} />
+                  <Trash size={24} className="text-gray-300 cursor-pointer" onClick={() => handleTaskDeletion(index)} />
                 </div>
               </div>
             ))}
           </div>
+          ) 
+          : 
+          (
+            <div className="flex-col items-center flex w-full md:w-2/4 mt-4 text-center">
+              <ClipboardText size={56} className="text-gray-300 mb-4" />
+              <h2 className="font-bold text-gray-300">Você ainda não tem tarefas cadastradas</h2>
+              <p className="font-normal text-gray-300">Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          )}
+          
         </div>
       </section>
     </div>
