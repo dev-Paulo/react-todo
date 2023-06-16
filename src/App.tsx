887dead6d6c85ec41caca6680ba2576ef949ff01
Paulo Renato <dev_paulo@outlook.com>
2023-06-16 11:34:45 -0300

    project done

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,45 @@
-
 import logoTodo from "./assets/Logo.svg";
 import "./App.css";
 import { ClipboardText, PlusCircle, Trash } from "@phosphor-icons/react";
 import { FormEvent, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-function App() {
+export default function App() {
   interface Task {
     id: number;
     name: string;
   }
 
-  const [tasks, setTasks] = useState<Task[]>([]); 
+  const newTaskSchema = z.object({
+    taskTitle: z.string().nonempty("É necessário um título para uma task"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Record<string, string>>({
+    resolver: zodResolver(newTaskSchema),
+  });
+
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [isTaskCompleted, setIsTaskCompleted] = useState<boolean[]>([]);
   const [completedTasksCount, setCompletedTasksCount] = useState(0);
   const [newTaskTitle, setNewTaskTitle] = useState("");
 
   useEffect(() => {
-    setCompletedTasksCount(isTaskCompleted.filter((completed) => completed).length);
+    setCompletedTasksCount(
+      isTaskCompleted.filter((completed) => completed).length
+    );
   }, [isTaskCompleted]);
 
-  const handleAddTask = (event: FormEvent) => {
-    event.preventDefault();
+  const handleAddTask = (data: Record<string, string>) => {
+    // event.preventDefault();
     setTasks([...tasks, { id: tasks.length + 1, name: newTaskTitle }]);
     setNewTaskTitle("");
+    console.log(data);
   };
 
   const handleCheckboxChange = (index: number) => {
@@ -55,24 +72,41 @@ function App() {
       </header>
       <section className="bg-gray-600 flex-grow flex-col flex px-4">
         <div className="flex-col mt-[-20px] items-center flex ">
-          <form onSubmit={handleAddTask} className="flex-row justify-center items-center flex w-full md:w-2/4 ">
-            <input
-              type="text"
-              name="taskTitle"
-              value={newTaskTitle}
-              onChange={(event) => setNewTaskTitle(event.target.value)}
-              className="bg-gray-500 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder:text-gray-300"
-              placeholder="Adicione uma nova tarefa"
-            />
-            <button
-              type="submit"
-              className="text-center inline-flex items-center text-white bg-blue-600 hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 ml-2 focus:outline-none dark:focus:ring-blue-800"
-            >
-              Criar
-              <PlusCircle size={20} className="ml-2" />
-            </button>
+          <form
+            onSubmit={handleSubmit(handleAddTask)}
+            className=" flex w-full md:w-2/4"
+          >
+           
+              <div className="flex w-full flex-col ">
+                <input
+                  type="text"
+                  // name="taskTitle"
+                  // value={newTaskTitle}
+                  {...register("taskTitle")}
+                  onChange={(event) => setNewTaskTitle(event.target.value)}
+                  className={`h-14 bg-gray-500 text-white text-sm rounded-lg  block w-full p-2.5 placeholder:text-gray-300 ${
+                    errors.taskTitle ? "border-red-500" : ""
+                  }`}
+                  placeholder="Adicione uma nova tarefa"
+                />
+                {errors.taskTitle && (
+                  <p className="text-red-500 text-md mt-2">
+                    {errors.taskTitle.message}
+                  </p>
+                )}
+              </div>
+
+              <button
+                type="submit"
+                className="h-14 text-center inline-flex items-center text-white bg-blue-600 hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 ml-2 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Criar
+                <PlusCircle size={20} className="ml-2" />
+              </button>
+           
           </form>
-          <div className="flex-row inline-flex justify-between mt-10 w-full md:w-2/4 mb-4">
+
+          <div className="flex-row inline-flex justify-between mt-16 w-full md:w-2/4 mb-4">
             <div className="flex-row inline-flex items-center w-2/4">
               <p className="text-blue-500 font-bold mr-2">Tarefas criadas</p>
               <span className="bg-gray-300 text-white text-xs font-medium px-2.5 py-0.5 rounded-full">
@@ -88,46 +122,56 @@ function App() {
           </div>
           {tasks.length > 0 ? (
             <div className="flex-col items-center flex w-full md:w-2/4">
-            {tasks.map((task: Task, index: number) => (
-              <div key={task.id} className="h-[72px] mb-4 p-4 rounded-lg bg-gray-500 flex-row inline-flex items-center w-full justify-between">
-                <div className="flex-row inline-flex items-center">
-                  <input
-                    id={`checkbox-${task.id}`}
-                    type="checkbox"
-                    value=""
-                    className="w-5 h-5 mr-2 text-purple-dark bg-gray-500 border-blue-500 rounded-xl focus:ring-purple-purple focus:ring-3 ring-blue-500 cursor-pointer"
-                    onChange={() => handleCheckboxChange(index)}
-                  />
-                  <p
-                    className="text-gray-100"
-                    style={{
-                      textDecoration: `${isTaskCompleted[index] ? "line-through" : ""}`,
-                      color: `${isTaskCompleted[index] ? "#808080" : "#F2F2F2"}`,
-                    }}
-                  >
-                    {task.name}
-                  </p>
+              {tasks.map((task: Task, index: number) => (
+                <div
+                  key={task.id}
+                  className="h-[72px] mb-4 p-4 rounded-lg bg-gray-500 flex-row inline-flex items-center w-full justify-between"
+                >
+                  <div className="flex-row inline-flex items-center">
+                    <input
+                      id={`checkbox-${task.id}`}
+                      type="checkbox"
+                      value=""
+                      className="w-5 h-5 mr-2 text-purple-dark bg-gray-500 border-blue-500 rounded-xl focus:ring-purple-purple focus:ring-3 ring-blue-500 cursor-pointer"
+                      onChange={() => handleCheckboxChange(index)}
+                    />
+                    <p
+                      className="text-gray-100"
+                      style={{
+                        textDecoration: `${
+                          isTaskCompleted[index] ? "line-through" : ""
+                        }`,
+                        color: `${
+                          isTaskCompleted[index] ? "#808080" : "#F2F2F2"
+                        }`,
+                      }}
+                    >
+                      {task.name}
+                    </p>
+                  </div>
+                  <div className="">
+                    <Trash
+                      size={24}
+                      className="text-gray-300 cursor-pointer"
+                      onClick={() => handleTaskDeletion(index)}
+                    />
+                  </div>
                 </div>
-                <div className="">
-                  <Trash size={24} className="text-gray-300 cursor-pointer" onClick={() => handleTaskDeletion(index)} />
-                </div>
-              </div>
-            ))}
-          </div>
-          ) 
-          : 
-          (
+              ))}
+            </div>
+          ) : (
             <div className="flex-col items-center flex w-full md:w-2/4 mt-4 text-center">
               <ClipboardText size={56} className="text-gray-300 mb-4" />
-              <h2 className="font-bold text-gray-300">Você ainda não tem tarefas cadastradas</h2>
-              <p className="font-normal text-gray-300">Crie tarefas e organize seus itens a fazer</p>
+              <h2 className="font-bold text-gray-300">
+                Você ainda não tem tarefas cadastradas
+              </h2>
+              <p className="font-normal text-gray-300">
+                Crie tarefas e organize seus itens a fazer
+              </p>
             </div>
           )}
-          
         </div>
       </section>
     </div>
   );
 }
-
-export default App;
